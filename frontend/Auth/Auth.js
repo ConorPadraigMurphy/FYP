1b9f3182dcc80a184794cd29a9f03518e3fcba7a
Rohan Sikder <rohansikder4@gmail.com>
2024-03-07 18:13:19 +0000

    Using bcrypt, passwords is hashed when user details sent to mongo and when logging in bcrypt verifies if the hashed input matches the stored hash

--- frontend/Auth/Auth.js
+++ frontend/Auth/Auth.js
@@ -1,43 +1,58 @@
+// Hashing library
+const bcrypt = require("bcryptjs")
 const User = require("../model/User")
 
 // Validations of register data and create or error
 exports.register = async (req, res, next) => {
-    const { username, password } = req.body
+    const { username, password } = req.body;
     if (password.length < 6) {
-        return res.status(400).json({ message: "Password less than 6 characters" })
+        return res.status(400).json({ message: "Password less than 6 characters" });
     }
     try {
-        await User.create({
+        // Hash the password with salt round (How many times hashing process is done higher the more brute force required but also time consuming to make)
+        const hash = await bcrypt.hash(password, 10);
+        // Create the user with the hashed password
+        const user = await User.create({
             username,
-            password,
-        }).then(user =>
-            res.status(200).json({
-                message: "User successfully created",
-                user,
-            })
-        )
+            password: hash,
+        });
+        res.status(200).json({
+            message: "User successfully created",
+            user,
+        });
     } catch (err) {
         res.status(401).json({
             message: "User not successfully created",
             error: err.message,
         });
     }
-}
+};
 
 // Looks for user in the user db using details with validation
 exports.login = async (req, res, next) => {
-    const { username, password } = req.body;
+    const { username, password } = req.body
+    // Check if username and password is entered
+    if (!username || !password) {
+        return res.status(400).json({
+            message: "Please enter Username or Password",
+        })
+    }
     try {
-        const user = await User.findOne({ username, password })
+        const user = await User.findOne({ username })
         if (!user) {
-            res.status(401).json({
+            res.status(400).json({
                 message: "Login not successful",
                 error: "User not found",
             })
         } else {
-            res.status(200).json({
-                message: "Login successful",
-                user,
+            // comparing given password with hashed password
+            bcrypt.compare(password, user.password).then(function (result) {
+                result
+                    ? res.status(200).json({
+                        message: "Login successful",
+                        user,
+                    })
+                    : res.status(400).json({ message: "Login not successful" })
             })
         }
     } catch (error) {
