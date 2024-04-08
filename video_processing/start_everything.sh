828dd796af559c3743d531ddd9cfc61a25251628
Rohan Sikder <rohansikder4@gmail.com>
2024-04-08 14:59:09 +0100

    Adding logs to see if python is getting run in deployment

--- video_processing/start_everything.sh
+++ video_processing/start_everything.sh
@@ -24,9 +24,10 @@ echo "Setting up Kafka topics..."
 echo "Starting Flask application..."
 export FLASK_APP=app.py
 export FLASK_ENV=development
-flask run > flask.log 2>&1 &
+flask run &
 
 echo "Running video_consumer.py script..."
-python3 video_consumer.py > video_consumer.log 2>&1 &
+python3 video_consumer.py &
+
 
 echo "All processes started."
