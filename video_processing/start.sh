1d4cea1fb37d7dca6870ed6065053200736cb09c
Rohan Sikder <rohansikder4@gmail.com>
2024-04-15 08:08:43 +0100

    Addeing wait to end of the start.sh so processing python script stays on

--- video_processing/start.sh
+++ video_processing/start.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-# Start the Flask app
-gunicorn app:app &
+# Start the Flask app using Gunicorn in the background
+gunicorn app:app --daemon
 
 # Wait for a few seconds to ensure Flask app starts first
 sleep 5
 
 # Start the video_consumer.py script
 python3 video_consumer.py
+
+# Keep the script running
+wait
